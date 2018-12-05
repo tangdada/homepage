dde552a6b676dab7265e6d99914ab3e0743c72cd
Jack-PC\Jack <361134446@qq.com>
2018-12-05 18:23:46 +0800

    fix

--- src/page/employee/employee.vue
+++ src/page/employee/employee.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="banner">
-      <div class="banner-cont">
+      <div class="banner-cont down">
         <div class="banner-title">医院人员情况表</div>
         <div class="banner-subhead">健康管理中心现有主检医师2名、内科12名、外科10名、妇科5名、眼科6名、耳鼻喉科4名、口腔科4名、心电图2名、B超13名，放射9名、抽血检验20名、测血压2名，身高体重2名，导检护士20。其中高级职称23人，中级职称28人，体检各岗位人员配置数量、职称和经验完全满足招标要求。</div>
       </div>
