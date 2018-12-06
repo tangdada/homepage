cfd0a25b83de8157abe965dfe5295e3939459006
Jack-PC\Jack <361134446@qq.com>
2018-12-06 10:32:02 +0800

    ip

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // API 后端服务地址配置
 const baseUrl = {
   // 开发环境
-  dev: 'http://localhost:8080',
+  dev: 'http://192.168.5.12:8080',
   // 测试环境
   test: window.useCustomizedPath ? 'http://crm-testb.mytijian.cn' : '',
   // 线上环境
